e92855b364fa1d1e6282f902387bb8bffff4b247
Shelly-XD <87390227+Shelly-XD@users.noreply.github.com>
2021-08-05 00:13:37 +0700

    Create Noah.php

---
+++ Noah.php
@@ -0,0 +1,11 @@
+<?php
+/*
+** Obfuscator by Rozhak
+** Obfuscator version : 1.0
+** Description: Kalo mau recode izin dulu bosss !!!
+_________________________________
+*/
+eval (gzinflate("í.ÝnÓ0^X..»«.¢^]lR.º²uC^S^G0q^E\^@jÚ@+¦nJZ¶.¸.Ɏ.ıãü8Î
+Ï8Ø×^Kø\x27^K؇.^\a.U.zqËq^_RÅ.#/.^B.[.b.²^[Ò^C.^A.^P.u.a.ÃÃo{.ÁÀ±×^[ge
+¡æ¦k©]ß8ú*ÉPÏÏ3Кºöä¤;^B¯綦ódî.?^@"));
+$. ..¦... ..¨....©¨¢.........«©.¦......©¦.¥....¡.£.¨....¤.ge¤..§¡.....
